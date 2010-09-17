1e2c5063b267f59394e696434e40aa149245e93a
rmohan <rmohan@rmohan-laptop.(none)>
2010-09-17 15:14:07 -0400

    changes to the home page to add new search/map tab

--- artworks/urls.py
+++ artworks/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('artworks.views',
     (r'range/(?P<year_from>\d+)/to/(?P<year_to>\d+)/$', 'in_range'),
-    url(r'records/(?P<artwork_id>\d+)/$', 'artworks_record', name="artworks_record"),
-    url(r'records/list/', 'artworks_list', name="artworks_list"),
+    url(r'(?P<artwork_id>\d+)/$', 'artworks_record', name="artworks_record"),
     (r'(?P<artwork_id>\d+)/$', 'properties'),
-        
+    url(r'^$', 'artworks_list', name="artworks_list"),        
 )
--- creators/urls.py
+++ creators/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('creators.views',
-    url(r'records/(?P<creator_id>\d+)/$', 'creator_record', name="creator_record"),
-    url(r'records/list/', 'creators_list', name="creators_list"),
+    url(r'(?P<creator_id>\d+)/$', 'creator_record', name="creator_record"),
+    url(r'^$', 'creators_list', name="creators_list"),
 )
