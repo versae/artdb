5ab543cd526cf9209f9532ff1e0e7603f24c5dcc
Javier de la Rosa <versae@gmail.com>
2010-10-02 20:20:35 -0400

    Fixed an error with place_map and geometry fields in places from artworks objects

--- base/templatetags/place_map_tags.py
+++ base/templatetags/place_map_tags.py
@@ -7,7 +7,7 @@ register = Library()
 
 
 @register.inclusion_tag('templatetags/place_map.html', takes_context=True)
-def place_map(context, geometry, point):
+def place_map(context, point):
     points = [point]
     img_src = url_map_points(points, width=250, height=200, create_path=True,
                              marker_size='small')
