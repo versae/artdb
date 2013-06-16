05f96faabbe8500a2b1048003d88341f34af0aff
Javier de la Rosa <versae@gmail.com>
2013-06-16 19:39:04 -0400

    Update load/dump scripts and requirements

--- backups/load.sh
+++ backups/load.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# In case of fail, apply the next patch
+# https://code.djangoproject.com/attachment/ticket/16778/postgis-adapter.patch
 ./manage.py loaddata backups/auth.json
 ./manage.py loaddata backups/base.json
 ./manage.py loaddata backups/creators.json
